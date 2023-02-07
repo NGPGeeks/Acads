0a4687e2a66b2d2d8ebd260b0ab0f2a2a2f6c49d
C.Lokesh Swarup <111900256+lokeshswarup@users.noreply.github.com>
2023-02-07 11:35:08 +0530

    Create Week2_LabTask1a.java

---
+++ JavaLab/Week2_LabTask1a.java
@@ -0,0 +1,17 @@
+package Week02;
+import java.util.Scanner;
+
+public class LabTask1a {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int rows;
+		System.out.print("Enter the number of rows:");
+		rows = input.nextInt();
+		for(int i=0;i<rows;i++)
+		{
+			System.out.println("**********");
+		}
+				
+	}
+
+}
